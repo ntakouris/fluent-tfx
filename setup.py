1e958cab414d3ba10213d658239ed8c6e46be7c6
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-07-29 23:32:19 +0300

    setup: add long description

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import setuptools
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setuptools.setup(
     name="fluent-tfx",
     version="0.0.3",
     author="Theodoros Ntakouris",
     author_email="zarkopafilis@gmail.com",
     description="A fluent API layer for tensorflow extended e2e machine learning pipelines",
-    long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     url="https://github.com/ntakouris/fluent-tfx",
     packages=setuptools.find_packages(),
     classifiers=[
